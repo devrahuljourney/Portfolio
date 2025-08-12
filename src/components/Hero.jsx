57aa506c7d3a961bd12e873703f15bec27749815
rahulkumarv269 <rahulkumarv269@gmail.com>
2025-08-12 17:14:00 +0530

    First

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+// Hero.tsx
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="text-[25px] text-[rgb(185,253,80)] text-center">
+      Hero
+    </div>
+  );
+}
